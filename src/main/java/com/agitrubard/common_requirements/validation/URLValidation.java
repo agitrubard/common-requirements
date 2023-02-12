6aaa5ba6df858e9f4eb69ea4eca00ffa99277640
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-02-12 17:56:18 +0300

    URL Validation Annotation Has Been Created

---
+++ src/main/java/com/agitrubard/common_requirements/validation/URLValidation.java
@@ -0,0 +1,18 @@
+package com.agitrubard.common_requirements.validation;
+
+import org.apache.commons.validator.routines.UrlValidator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+class URLValidation implements ConstraintValidator<URL, Object> {
+
+    public boolean isValid(final Object value, final ConstraintValidatorContext context) {
+        if (value == null) return true;
+
+        final String[] schemes = {"http", "https"};
+        final UrlValidator urlValidator = new UrlValidator(schemes);
+
+        return urlValidator.isValid(value.toString());
+    }
+}
